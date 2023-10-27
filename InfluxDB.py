3d7211b87abf1a42dfc1edfdbec13092709de1cb
Matzmuda4 <130379770+Matzmuda4@users.noreply.github.com>
2023-10-27 16:01:01 +0200

    InfluxDB.py

--- InfluxDB.py
+++ InfluxDB.py
@@ -1,41 +1,40 @@
-import influxdb_client, os, time
+import influxdb_client
 from sqlalchemy import create_engine
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-
-token = os.environ.get("INFLUXDB_TOKEN")
+import pandas as pd
+import os
+# InfluxDB settings
+token = "Jh4tKpQc9wRvXzYqM1Nl6xT5rVpQaH9szzZldHaTX4t9Tj1UpGIsiUOgt2zhTwPGcxNm7ZcFtugA=="
 org = "Mat"
 url = "http://localhost:8086"
-write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-bucket = "Designing Databases"
+bucket = "Databases"
 
+
+# Initialize InfluxDB client
 client = InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-query = """
-    select f.film_id, f.title, sum(p.amount), count(*) as total_revenue
-    FROM film f 
-    JOIN inventory i ON f.film_id = i.film_id
-    JOIN rental r ON i.inventory_id = r.inventory_id
-    JOIN payment p on r.rental_id = p.rental_id
-    group by f.title, f.film_id
-    limit 5; 
-    """
+# Initialize SQLAlchemy engine for MySQL
 engine = create_engine('mysql://root:@localhost/sakila')
 
-# Connect to the MySQL database using SQLAlchemy
-with engine.connect() as connection:
-    result = connection.execute(query)
+# Your SQL query
+query = """
+    SELECT customer_id, count(*) as num_rentals 
+    FROM rental 
+    GROUP BY customer_id 
+    ORDER BY num_rentals desc 
+    LIMIT 5; 
+"""
+data = pd.read_sql(query, engine)
+points = []
+# Create a point for each data row
+for row in data.itertuples(index=False):
+    point = Point("rental").tag("customer_id", row.customer_id).field("num_rentals", row.num_rentals)
+    points.append(point)
+# Write points to InfluxDB
+write_api.write(bucket=bucket, record=points, write_precision=WritePrecision.NS)
 
-    for row in result:
-        title = row['title']  # Extracting the customer_id
-        total_revenue = row['total_revenue']  # Extracting the num_rentals
-
-        # Create a point and write it to InfluxDB
-        point = (
-            Point("Top grossing movies")
-            .tag("title", (title))
-            .field("total_revenue", total_revenue)
-        )
-        write_api.write(bucket=bucket, org=org, record=point)
+# Close the client
+client.close()
 
